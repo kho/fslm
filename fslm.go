abdd2913a2dd40d82845e06053619035bde29dd0
Wu, Ke <wuke@cs.umd.edu>
2014-04-27 03:23:56 -0400

    Build full-depth trie and then pre-walk the back-off chain
    The previous algorithm is wrong when some suffix of an added
    higher-order n-gram is not an added lower-order n-gram. For example,
    when "<s> a a a a" is in a 5-gram model while "a a a" is not. The old
    algorithm creates state "a a a a" regardless and leads to free "a a a"
    transition (e.g. in "<s> <unk> a a a", the last transition should have
    score log(0) instead of 0). Building full-depth trie solves this
    problem at the price of using more memory (the number of states
    initially might double; the number of transitions remains unchanged
    however).
    
    As a first step to later reduce the number of states, pre-walking the
    back-off chain is implemented. In *Builder.link(), now we link a state
    to its first back-off state with at least one lexical transition,
    summing the weights along the way.

--- fslm.go
+++ fslm.go
@@ -2,6 +2,7 @@ package fslm
 
 import (
 	"fmt"
+	"io"
 	"log"
 )
 
@@ -17,12 +18,15 @@ const (
 // constructed using NewVocab() so that WORD_UNK, WORD_BOS and
 // WORD_EOS are populated properly.
 type Vocab struct {
-	Unk, Bos, Eos string // For obvious reason the user should not modify these.
+	Unk, BOS, EOS string // For obvious reason the user should not modify these.
 	id2str        []string
 	str2id        map[string]WordId
 }
 
 func NewVocab(unk, bos, eos string) *Vocab {
+	if unk == bos || unk == eos || bos == eos {
+		panic("NewVocab: unk, bos, and eos can not be the same")
+	}
 	// We know WORD_UNK = 0...
 	id2str := []string{WORD_UNK: unk, WORD_BOS: bos, WORD_EOS: eos}
 	str2id := map[string]WordId{unk: WORD_UNK, bos: WORD_BOS, eos: WORD_EOS}
@@ -92,6 +96,14 @@ type state struct {
 
 // map queries are much faster with this than a struct of two uint32s.
 type srcWord uint64
+
+func (i srcWord) Src() StateId {
+	return StateId(i >> 32)
+}
+
+func (i srcWord) Word() WordId {
+	return WordId(i & 0xFFFFFFFF)
+}
 
 func newSrcWord(s StateId, w WordId) srcWord {
 	return srcWord(s)<<32 | srcWord(w)
@@ -181,28 +193,34 @@ func (m *Model) Final(p StateId) Weight {
 	return w
 }
 
+func (m *Model) Graphviz(w io.Writer) {
+	fmt.Fprintln(w, "digraph {")
+	fmt.Fprintln(w, "  // lexical transitions")
+	for px, qw := range m.transitions {
+		fmt.Fprintf(w, "  %d -> %d [label=%q]\n", px.Src(), qw.Tgt, fmt.Sprintf("%s : %g", m.Vocab.StringOf(px.Word()), qw.Weight))
+	}
+	fmt.Fprintln(w, "  // back-off transitions")
+	for i, s := range m.states {
+		fmt.Fprintf(w, "  %d -> %d [label=%q,style=dashed]\n", i, s.BackOffState, fmt.Sprintf("%g", s.BackOffWeight))
+	}
+	fmt.Fprintln(w, "}")
+}
+
 // Builder builds a Model from n-grams (e.g. estimated by SRILM). Must
 // be constrcuted using NewBuilder().
 type Builder struct {
-	order  int
 	vocab  *Vocab
 	states []state
 	nexts  []map[WordId]tgtWeight
 }
 
-// NewBuilder constrcuts a new Builder. order is the maximum order of
-// n-grams (i.e. n-1). vocab is the base vocabulary to use for the
-// resulting Model; it can be nil, in which case a vocabulary with
-// [<unk>, <s>, </s>] as [Unk, Bos, Eos]. Subsequent calls from
-// Builder will not modify vocab (i.e. a copy is made when vocab !=
-// nil).
-func NewBuilder(order int, vocab *Vocab) *Builder {
-	if order <= 0 {
-		panic(fmt.Sprintf("LM order should be postive; not %d", order))
-	}
-
+// NewBuilder constrcuts a new Builder. vocab is the base vocabulary
+// to use for the resulting Model; it can be nil, in which case a
+// vocabulary with [<unk>, <s>, </s>] as [Unk, Bos, Eos]. Subsequent
+// calls from Builder will not modify vocab (i.e. a copy is made when
+// vocab != nil).
+func NewBuilder(vocab *Vocab) *Builder {
 	var builder Builder
-	builder.order = order
 	if vocab == nil {
 		builder.vocab = NewVocab("<unk>", "<s>", "</s>")
 	} else {
@@ -211,6 +229,7 @@ func NewBuilder(order int, vocab *Vocab) *Builder {
 	// _STATE_EMPTY and _STATE_START.
 	builder.newState()
 	builder.newState()
+	builder.setTransition(_STATE_EMPTY, WORD_BOS, _STATE_START, 0)
 	return &builder
 }
 
@@ -218,38 +237,29 @@ func NewBuilder(order int, vocab *Vocab) *Builder {
 // or this will panic. For other problematic input, warnings will be
 // logged.
 func (b *Builder) AddNGram(context []string, word string, weight Weight, backOff Weight) {
-	if len(context) > b.order {
-		panic(fmt.Sprintf("adding %d-gram to order-%d LM: %q -> %q", len(context)+1, b.order, context, word))
-	}
-	if len(context) > 0 && word == b.vocab.Bos && weight > -10 {
+	if len(context) > 0 && word == b.vocab.BOS && weight > -10 {
 		log.Printf("there is a non-unigram ending in %q with weight %g (such n-gram should have -inf weight or not occur in the LM)", word, weight)
 	}
-	if word == b.vocab.Eos && backOff != 0 {
+	if word == b.vocab.EOS && backOff != 0 {
 		log.Printf("non-zero back-off %g for a n-gram ending in %q", backOff, word)
 	}
 
 	p := b.findState(_STATE_EMPTY, context)
-	wordI := b.vocab.IdOrAdd(word)
-	if len(context) < b.order {
-		q := b.findNextState(p, wordI)
-		b.setTransition(p, wordI, q, weight)
+	x := b.vocab.IdOrAdd(word)
+	q := STATE_NIL
+	// Only use a valid destination state when word is not EOS.
+	if x != WORD_EOS {
+		q = b.findNextState(p, x)
 		b.setBackOffWeight(q, backOff)
-	} else {
-		q := b.findNextState(b.findState(_STATE_EMPTY, context[:len(context)-1]), wordI)
-		b.setTransition(p, wordI, q, weight)
-		if backOff != 0 {
-			log.Printf("ignoring the non-zero back-off weight of a highest-order n-gram: %q -> %q", context, word)
-		}
 	}
+	b.setTransition(p, x, q, weight)
 }
 
 func (b *Builder) newState() StateId {
 	s := StateId(len(b.states))
-	// Initialize the back-off state to 0, which is the back-off state
-	// for unigram contexts.
-	//
-	// TODO: 0 or WEIGHT_LOG0 for back-off weight?
-	b.states = append(b.states, state{})
+	// Back-off is initialized to STATE_NIL to signify an "unknown"
+	// back-off.
+	b.states = append(b.states, state{STATE_NIL, 0})
 	b.nexts = append(b.nexts, map[WordId]tgtWeight{})
 	return s
 }
@@ -283,30 +293,69 @@ func (b *Builder) findState(p StateId, ws []string) StateId {
 // b. Subsequent calls to b.AddNGram() will have undefined behavior
 // (probably panic).
 func (b *Builder) Dump() *Model {
-	b.linkBackOff()
+	b.link()
 	return b.move()
 }
 
-func (b *Builder) linkBackOff() {
-	b.states[_STATE_EMPTY].BackOffState = STATE_NIL
-	// For safety...
+// link links each state p to the first state q with at least one
+// lexical transition along p' back-off chain.
+func (b *Builder) link() {
+	// For safety.
 	if _STATE_EMPTY != 0 || _STATE_START != 1 {
 		panic("this assumes _STATE_EMPTY == 0 and _STATE_START == 1")
 	}
-	// newState has already set up the back-off states for children of
-	// _STATE_EMPTY. So start with the next guy (i.e. _STATE_START).
-	for i, s := range b.states[_STATE_START:] {
-		for x, qw := range b.nexts[StateId(i)+_STATE_START] {
-			q := qw.Tgt
-			pBack := s.BackOffState
-			pBackQw, ok := b.nexts[pBack][x]
-			for !ok && pBack != _STATE_EMPTY {
-				pBack = b.states[pBack].BackOffState
-				pBackQw, ok = b.nexts[pBack][x]
-			}
-			b.states[q].BackOffState = pBackQw.Tgt
+	// Children of _STATE_EMPTY directly backs off the _STATE_EMPTY.
+	for _, qw := range b.nexts[_STATE_EMPTY] {
+		if qw.Tgt != STATE_NIL {
+			b.states[qw.Tgt].BackOffState = _STATE_EMPTY
 		}
 	}
+	// States are created with STATE_NIL as the default back-off. Except
+	// for _STATE_EMPTY, having a STATE_NIL back-off means the back-off
+	// is yet to be computed.
+	for i, next := range b.nexts[1:] {
+		for x, qw := range next {
+			p, q := StateId(i+1), qw.Tgt
+			if q != STATE_NIL {
+				b.linkTransition(p, x, q)
+			}
+		}
+	}
+}
+
+// linkTransition recursively link q to the lowest back-off state with
+// at least one lexical transition. q must not be _STATE_EMPTY. This
+// function might change q's back-off weight when the final back-off
+// state is not q's immediately back-off.
+func (b *Builder) linkTransition(p StateId, x WordId, q StateId) (StateId, Weight) {
+	qState := &b.states[q]
+	if qState.BackOffState == STATE_NIL {
+		// Find the next back-off state.
+		pBack := b.states[p].BackOffState
+		qwBack, ok := b.nexts[pBack][x]
+		for !ok && pBack != _STATE_EMPTY {
+			pBack := b.states[pBack].BackOffState
+			qwBack, ok = b.nexts[pBack][x]
+		}
+		if ok {
+			qBack := qwBack.Tgt
+			// pBack is not STATE_NIL; qBack is not _STATE_EMPTY. We can go
+			// back further.
+			qBackBack, w := b.linkTransition(pBack, x, qBack)
+			if len(b.nexts[qBack]) == 0 {
+				qState.BackOffState = qBackBack
+				// We are skipping the transition from qBack to qBackBack,
+				// thus its weight needs to be included in our back-off weight
+				// as well.
+				qState.BackOffWeight += w
+			} else {
+				qState.BackOffState = qBack
+			}
+		} else {
+			qState.BackOffState = _STATE_EMPTY
+		}
+	}
+	return qState.BackOffState, qState.BackOffWeight
 }
 
 func (b *Builder) move() *Model {
@@ -317,7 +366,7 @@ func (b *Builder) move() *Model {
 	for i, n := range b.nexts {
 		b.nexts[i] = nil // Allow GC to reclaim this map after this iteration.
 		for x, qw := range n {
-			m.transitions[newSrcWord(StateId(i), x)] = qw
+			m.transitions[newSrcWord(StateId(i), x)] = tgtWeight{qw.Tgt, qw.Weight}
 		}
 	}
 	return &m
---
+++ fslm_test.go
@@ -0,0 +1,183 @@
+package fslm
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestVocab(t *testing.T) {
+	v := NewVocab("<unk>", "<s>", "</s>")
+
+	if b := v.Bound(); b != 3 {
+		t.Errorf("expected v.Bound() = 3; got %d", b)
+	}
+
+	x := v.IdOrAdd("x")
+	v1, v2 := v.Copy(), v.Copy()
+	v1.IdOrAdd("a")
+	v2.IdOrAdd("b")
+
+	for _, i := range []struct {
+		S string
+		I WordId
+	}{
+		{"<unk>", WORD_UNK}, {"<s>", WORD_BOS}, {"</s>", WORD_EOS}, {"x", x}, {"y", WORD_UNK},
+	} {
+		if a := v.IdOf(i.S); a != i.I {
+			t.Errorf("expected v.IdOf(%q) = %d; got %d", i.S, i.I, a)
+		}
+		if i.I == WORD_UNK && i.S != "<unk>" {
+			i.S = "<unk>"
+		}
+		if a := v.StringOf(i.I); a != i.S {
+			t.Errorf("expected v.StringOf(%d) = %q; got %q", i.I, i.S, a)
+		}
+	}
+
+	if b := v1.IdOf("b"); b != WORD_UNK {
+		t.Errorf("expected v1.IdOf(%q) = %d; got %d", "b", WORD_UNK, b)
+	}
+	if a := v2.IdOf("a"); a != WORD_UNK {
+		t.Errorf("expected v2.IdOf(%q) = %d; got %d", "b", WORD_UNK, a)
+	}
+	if a := v.IdOf("a"); a != WORD_UNK {
+		t.Errorf("expected v.IdOf(%q) = %d; got %d", "a", WORD_UNK, a)
+	}
+	if b := v.IdOf("b"); b != WORD_UNK {
+		t.Errorf("expected v.IdOf(%q) = %d; got %d", "a", WORD_UNK, b)
+	}
+
+	v.IdOrAdd("y")
+	if y := v1.IdOf("y"); y != WORD_UNK {
+		t.Errorf("expected v1.IdOf(%q) = %d; got %d", "y", WORD_UNK, y)
+	}
+	if y := v2.IdOf("y"); y != WORD_UNK {
+		t.Errorf("expected v2.IdOf(%q) = %d; got %d", "y", WORD_UNK, y)
+	}
+
+	y := v.IdOf("y")
+	if yy := v.IdOrAdd("y"); yy != y {
+		t.Errorf("expected v.IdOrAdd(%q) = %d; got %d", "y", y, yy)
+	}
+
+	if b := v.Bound(); b != 5 {
+		t.Errorf("expected v.Bound() = 5; got %d", b)
+	}
+
+	for _, i := range [][3]string{
+		{"a", "a", "c"}, {"a", "b", "a"}, {"a", "b", "b"},
+	} {
+		func() {
+			defer func() {
+				err := recover()
+				if err == nil {
+					t.Error("expected panic; got nil error")
+				}
+			}()
+			NewVocab(i[0], i[1], i[2])
+		}()
+	}
+}
+
+type ngram struct {
+	Context, Word   string
+	Weight, BackOff Weight
+}
+
+func (n ngram) Params() ([]string, string, Weight, Weight) {
+	var context []string
+	if n.Context != "" {
+		context = strings.Fields(n.Context)
+	}
+	return context, n.Word, n.Weight, n.BackOff
+}
+
+type token struct {
+	Word   string
+	Weight Weight
+}
+
+var simpleTrigramLM = []ngram{
+	{"", "<s>", WEIGHT_LOG0, -1},
+	{"", "</s>", -0.01, 0},
+	{"", "a", -2, -1},
+	{"", "b", -4, -2},
+	{"<s>", "a", -1, -0.5},
+	{"a", "b", -2, -1},
+	{"<s> a", "b", -1.5, 0},
+	{"a b", "</s>", -0.001, 0},
+}
+
+var simpleTrigramSents = [][]token{
+	{{"a", -1}, {"</s>", -0.5 - 1 - 0.01}},
+	{{"a", -1}, {"b", -1.5}, {"</s>", -0.001}},
+	{{"a", -1}, {"b", -1.5}, {"a", -1 - 2 - 2}, {"b", -2}, {"</s>", -0.001}},
+	{{"a", -1}, {"b", -1.5}, {"c", WEIGHT_LOG0}, {"</s>", -0.01}},
+}
+
+var sparseFivegramLM = []ngram{
+	{"", "<s>", WEIGHT_LOG0, -1},
+	{"", "</s>", 0.1, 0},
+	{"<s> a a a", "a", -1, -2},
+	{"a a", "a", -3, -4},
+}
+
+var sparseFivegramSents = [][]token{
+	{{"a", 0}, {"</s>", 0.1}},
+	{{"a", 0}, {"a", 0}, {"</s>", 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"</s>", -4 + 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"a", -1}, {"</s>", -2 - 4 + 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"a", -1}, {"a", -2 - 4 - 3}, {"</s>", -4 + 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"a", -1}, {"a", -2 - 4 - 3}, {"a", -4 - 3}, {"</s>", -4 + 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"a", -1}, {"a", -2 - 4 - 3}, {"a", -4 - 3}, {"a", -4 - 3}, {"</s>", -4 + 0.1}},
+}
+
+var sparserFivegramLM = []ngram{
+	{"", "<s>", WEIGHT_LOG0, -1},
+	{"", "</s>", 0.1, 0},
+	{"<s> a a a", "a", -1, -2},
+}
+
+var sparserFivegramSents = [][]token{
+	{{"a", 0}, {"</s>", 0.1}},
+	{{"a", 0}, {"a", 0}, {"</s>", 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"</s>", 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"a", -1}, {"</s>", -2 + 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"a", -1}, {"a", WEIGHT_LOG0}, {"</s>", 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"a", -1}, {"a", WEIGHT_LOG0}, {"a", WEIGHT_LOG0}, {"</s>", 0.1}},
+	{{"a", 0}, {"a", 0}, {"a", 0}, {"a", -1}, {"a", WEIGHT_LOG0}, {"a", WEIGHT_LOG0}, {"a", WEIGHT_LOG0}, {"</s>", 0.1}},
+}
+
+func lmTest(lm []ngram, sents [][]token, t *testing.T) {
+	builder := NewBuilder(nil)
+	for _, i := range lm {
+		c, x, w, b := i.Params()
+		builder.AddNGram(c, x, w, b)
+	}
+	model := builder.Dump()
+	var buf bytes.Buffer
+	model.Graphviz(&buf)
+	t.Log("LM:\n", buf.String())
+
+	for _, i := range sents {
+		p := model.Start()
+		w := Weight(99)
+		for j, x := range i {
+			if x.Word != "</s>" {
+				p, w = model.NextS(p, x.Word)
+			} else {
+				w = model.Final(p)
+			}
+			if w != x.Weight {
+				t.Errorf("expected weight = %g; got %g\nsent: %v\nword: %d@%v", x.Weight, w, i, j, x)
+			}
+		}
+	}
+}
+
+func TestLMs(t *testing.T) {
+	lmTest(simpleTrigramLM, simpleTrigramSents, t)
+	lmTest(sparseFivegramLM, sparseFivegramSents, t)
+	lmTest(sparserFivegramLM, sparserFivegramSents, t)
+}

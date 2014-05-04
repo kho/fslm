46925a02cecad0ad6a3157b726291f53dce16676
Wu, Ke <wuke@cs.umd.edu>
2014-05-04 17:31:40 -0400

    Profiling flags in compile

--- cmd/compile/compile.go
+++ cmd/compile/compile.go
@@ -6,14 +6,34 @@ import (
 	"github.com/kho/easy"
 	"github.com/kho/fslm"
 	"os"
+	"runtime/pprof"
 )
 
 func main() {
 	var args struct {
 		Out string `name:"out" usage:"output path"`
 	}
+	cpuprofile := flag.String("cpuprofile", "", "path to write CPU profile")
+	memprofile := flag.String("memprofile", "", "path to write memory profile")
 	scale := flag.Float64("fslm.scale", 1.5, "scale multiplier for deciding the hash table size")
 	easy.ParseFlagsAndArgs(&args)
+
+	if *cpuprofile != "" {
+		w := easy.MustCreate(*cpuprofile)
+		pprof.StartCPUProfile(w)
+		defer func() {
+			pprof.StopCPUProfile()
+			w.Close()
+		}()
+	}
+
+	if *memprofile != "" {
+		defer func() {
+			w := easy.MustCreate(*memprofile)
+			pprof.WriteHeapProfile(w)
+			w.Close()
+		}()
+	}
 
 	model, err := fslm.FromARPA(os.Stdin, *scale)
 	if err != nil {
